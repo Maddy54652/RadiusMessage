9fbac4ebce7e9cf411fb4b905837953e9b05a145
Maddy <37761862+Maddy54652@users.noreply.github.com>
2020-04-17 15:49:05 -0700

    Create console.sql

---
+++ sql/console.sql
@@ -0,0 +1,22 @@
+CREATE TABLE postArchive(
+
+
+)
+
+
+/*
+Original SQL stuff:
+ALTER TABLE post_archive
+ADD user_message_text LONGTEXT;
+
+ALTER TABLE post_archive
+ADD radius DOUBLE(2,1);
+
+ALTER TABLE post_archive
+ADD location_x DOUBLE(2,1);
+
+ALTER TABLE post_archive
+ADD location_y DOUBLE(2,1);
+
+ALTER TABLE post_archive
+ADD message_duration INT(255);*/
